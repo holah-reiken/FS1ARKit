53550eedcfb16d9dfb418657e328805ccde39166
Howard Olah-Reiken <howard@manahealth.com>
2018-02-15 15:21:30 -0500

    now can add multiple tracks based on camera position

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,6 +7,9 @@ public class Manager : MonoBehaviour {
 	public static Manager instance = null;
 
 	public Transform trackPiecePrefab;
+	public Transform ballPrefab;
+
+	public float scaleFactor = .5f;
 
 	void Awake()
 	{
@@ -25,15 +28,37 @@ public class Manager : MonoBehaviour {
 		//Sets this to not be destroyed when reloading scene
 		DontDestroyOnLoad (gameObject);
 	}
-
+		
 	void Update () {
 		if (Input.touchCount > 0 && trackPiecePrefab != null) {
 
-			Vector3 position = new Vector3 (0, 0, 1);
-			Transform trackPiece = Transform.Instantiate (trackPiecePrefab);
-			trackPiece.position = position;
+			var touch = Input.GetTouch(0);
 
-			Debug.Log ("trackPiece.position=" + trackPiece.position.ToString());
+			if (touch.phase == TouchPhase.Began) {
+
+				//Vector3 position = new Vector3 (0, 0, 1);
+
+				Vector3 touchPos = touch.position;
+				touchPos.z = 2;
+				Vector3 position = Camera.main.ScreenToWorldPoint(touchPos);
+
+				Transform trackPiece = Transform.Instantiate (trackPiecePrefab);
+				trackPiece.position = position;
+
+				Vector3 trackScale = trackPiece.localScale;
+				trackPiece.localScale = new Vector3 (trackScale.x * scaleFactor, trackScale.y * scaleFactor, trackScale.z * scaleFactor);
+				Debug.Log ("trackScale="+trackScale.ToString()+", trackPiece.localScale=" + trackPiece.localScale.ToString ());
+
+				Transform ball = Transform.Instantiate (ballPrefab);
+				ball.position = new Vector3 (position.x, position.y + 1f, position.z);
+
+				Vector3 ballScale = ball.localScale;
+				ball.localScale = new Vector3 (ballScale.x * scaleFactor, ballScale.y * scaleFactor, ballScale.z * scaleFactor);
+				Debug.Log ("ballScale="+ballScale.ToString()+", ball.localScale=" + ball.localScale.ToString ());
+
+				Debug.Log ("trackPiece.position=" + trackPiece.position.ToString () + ", touch.position=" + touch.position.ToString ());
+
+			}
 		}
 	}
 }

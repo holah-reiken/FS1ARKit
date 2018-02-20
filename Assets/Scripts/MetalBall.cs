34860f730bce9c5575d3ef64059e90894f0251f5
Howard Olah-Reiken <howard@manahealth.com>
2018-02-20 16:57:30 -0500

    started adding sounds

---
+++ Assets/Scripts/MetalBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalBall : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	public AudioClip hitSound;
+	public AudioClip rollSound;
+
+	// Use this for initialization
+	void Awake () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject.CompareTag("TrackParent")) {
+			StartCoroutine(Play());
+		}
+	}
+
+	void OnCollisionExit(Collision col) {
+		audioSource.Stop ();
+	}
+
+	IEnumerator Play() {
+
+		audioSource.clip = hitSound;
+		audioSource.Play ();
+		yield return new WaitForSeconds (1);
+		audioSource.clip = rollSound;
+		audioSource.Play ();
+	}
+}
